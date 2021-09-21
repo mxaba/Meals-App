c744e16aa13859c6afea2afb18f89e85b36e48c5
mxaba <samuel.mcebo@gmail.com>
2021-09-21 20:54:14 +0200

    about to add a method on tap

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -8,18 +8,23 @@ class CategoryItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(title,
-      style: Theme.of(context).textTheme.title,
-      ),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [color.withOpacity(0.7), color],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight
+    return InkWell(
+      onTap: ,
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(title,
+        style: Theme.of(context).textTheme.title,
         ),
-        borderRadius: BorderRadius.circular(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [color.withOpacity(0.7), color],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
       ),
     );
   }
